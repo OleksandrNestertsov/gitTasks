45a79a9c7362ded3f467f0d36e3ac1efaeae7178
gtZaskar <gtZaskarProLe@gmail.com>
2020-12-08 11:37:40 +0200

    added classWorks A-Level classes

---
+++ classWorksAlevel/calendarDayNumber/src/com/alevel/classWorksAlevel/tasks/calendarDayNumber/NewCalendarDayOfYearNumber.java
@@ -0,0 +1,21 @@
+package com.alevel.classWorksAlevel.tasks.calendarDayNumber;
+
+import java.util.Scanner;
+
+public class NewCalendarDayOfYearNumber {
+    public static void main(String[] args) {
+        System.out.println("January 1 is Monday");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the day number in year: ");
+        int day = scanner.nextInt();
+
+        int counter = 0;
+        int month = 1;
+        int daysInMonth =31;
+
+        int dayInYear = day%365;
+
+      /*  while (counter+daysInMonth<dayInYear){
+            counter +=*/
+        }
+}
---
+++ classWorksAlevel/calendarDayNumber/src/com/alevel/classWorksAlevel/tasks/equalsGCDLCM/findingLCM/FindingLCM.java
@@ -0,0 +1,24 @@
+package com.alevel.classWorksAlevel.tasks.equalsGCDLCM.findingLCM;
+
+import java.util.Scanner;
+
+public class FindingLCM {
+    public static void main(String[] args) {
+        System.out.println("write two natural numbers : ");
+        Scanner scanner = new Scanner(System.in);
+        long number1 = scanner.nextLong();
+        long number2 = scanner.nextLong();
+        long num1, num2;
+        long mult;
+        mult=number1*number2;
+
+        while (number1!=0 & number2!=0){
+            if (number1>number2){
+                number1= number1%number2;
+            }else{
+                number2= number2%number1;
+            }
+        }
+        System.out.println("LCM equal : " + (mult)/(number1+number2));
+    }
+}
