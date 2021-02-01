28e499e81a0b45b0ab08df75d577fdc8d72b9de0
gtZaskar <gtZaskarProLe@gmail.com>
2021-02-01 11:57:03 +0200

    addtd home task Find Fibonacci numbers

---
+++ alevelHomeWorks/src/main/java/com/alevel/alevelHomeWorks/fibonacci/Fibonacci.java
@@ -0,0 +1,10 @@
+package main.java.com.alevel.alevelHomeWorks.fibonacci;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Numbers fib = new Numbers();
+        System.out.println(fib.areFibNumbers(0, 1, 1, 2, 3, 5, 8, 13, 21,34));
+
+    }
+}
---
+++ alevelHomeWorks/src/main/java/com/alevel/alevelHomeWorks/fibonacci/Numbers.java
@@ -0,0 +1,45 @@
+package main.java.com.alevel.alevelHomeWorks.fibonacci;
+
+public class Numbers {
+    boolean num;
+
+    public boolean areFibNumbers(int... numbers) {
+        for (int i = 0; i < numbers.length - 2; i++) {
+
+            if (numbers[i] > numbers[i + 1] ||
+                    numbers[i + 1] > numbers[i + 2] ||
+                    numbers[i] + numbers[i + 1] != numbers[i + 2]) {
+                num = false;
+                break;
+            } else {
+                num = true;
+            }
+        }
+        return num;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
